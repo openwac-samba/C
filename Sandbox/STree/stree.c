e4295305e724fa64e5bc43f4a47c0f0c443d8fe8
SambaWS <openwac.samba@gmail.com>
2023-02-13 20:44:28 +0000

    Começo de dev da 'push_bt'

--- Sandbox/STree/stree.c
+++ Sandbox/STree/stree.c
@@ -8,3 +8,24 @@ typedef struct bt_node
     struct bt_node* l;
 }bt_node; 
 
+bt_node* push_bt (bt_node* n, void* key, void* content, int (* compare) (void*, void*), void (* insert) (void**, void*))
+{
+    if (!n)
+    {
+        n= new_node ();
+        insert (&n->content, content);
+        //Fix balance
+    }
+    else
+    {
+        int c= compare (n->key, key);
+        if (!(c= compare))                                                           //Compare determined the keys to match
+            insert (&n->content, content);
+        else
+            if (c> 0)
+                n->r= push_bt (n->r, key, content, compare, insert);
+            else
+                n->l= push_bt (n->l, key, content, compare, insert);
+    }
+    return n;
+}
