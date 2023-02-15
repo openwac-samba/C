f35d84a6416c833af09c952fc181c82c2771d37a
Samba.lnv <diogofontes2003@gmail.com>
2023-02-15 13:00:26 +0000

    dyn_array

--- Sandbox/STree/dyn_array.c
+++ Sandbox/STree/dyn_array.c
@@ -1,6 +1,24 @@
-typedef struct
+#define SIZE 10
+
+typedef struct dyn_array
 {
     int pos;
     int size;
-    void** array;
-}din_array;
+    void** a;
+}array;
+
+static array* init ()
+{
+    array* d= malloc (sizeof (array));
+    d->pos= 0;
+    d->size= SIZE;
+    d->a= malloc (SIZE* sizeof (array));
+    return d;
+}
+
+int push_array (array** a, void* elem, int (*insert) (void**, void*))
+{
+    if (!(*a))
+        *a= init ();
+    return insert ((*a)->a[(*a)->pos], elem);
+}
